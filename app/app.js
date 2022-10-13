237d0b51ae81b59d196d13859e0a81caa43d4e14
Emma <emersonpaige5@gmail.com>
2022-10-13 14:59:59 -0400

    js change

--- app/app.js
+++ app/app.js
@@ -1,29 +1,29 @@
-import * as MODEL from "./model.js";
+// import * as MODEL from "./model.js";
 
-function route() {
-    let hashTag = window.location.hash;
-    let pageID = hashTag.replace("#",""); 
-    let pageIDArray= pageID.split("/");
-    pageID = pageIDArray[0];
-    let subPageID = pageIDArray[1];
-    console.log("sub "+ subPageID);
+// function route() {
+//     let hashTag = window.location.hash;
+//     let pageID = hashTag.replace("#",""); 
+//     let pageIDArray= pageID.split("/");
+//     pageID = pageIDArray[0];
+//     let subPageID = pageIDArray[1];
+//     console.log("sub "+ subPageID);
     
-    if (pageID == "") {
-        MODEL.changePage("home");
-    } else {
-        if(pageID == subPageID) {
-        MODEL.changePage(pageID);
-    } else {
-        MODEL.changePage(pageID, subPageID);
-    }
-    }
-}
+//     if (pageID == "") {
+//         MODEL.changePage("home");
+//     } else {
+//         if(pageID == subPageID) {
+//         MODEL.changePage(pageID);
+//     } else {
+//         MODEL.changePage(pageID, subPageID);
+//     }
+//     }
+// }
 
-function initListeners () {
-    //$("nav a").click((e) => {
-      //  console.log("click "+ btnID);
-   // });
-}
+// function initListeners () {
+//     //$("nav a").click((e) => {
+//       //  console.log("click "+ btnID);
+//    // });
+// }
 
 //initialize listeners on url
 function initApp() {
